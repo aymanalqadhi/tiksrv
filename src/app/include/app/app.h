0bb0329e92bb3d35fe951a44519e4b43d0fc4810
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-13 00:03:45 +0300

    Document app module functions

--- src/app/include/app/app.h
+++ src/app/include/app/app.h
@@ -4,9 +4,25 @@
 #include "config/config.h"
 #include "log/error.h"
 
+/*!
+ * \brief Runs the application
+ *
+ * This function is the entry point of the application as whole. This function
+ * initializes and starts all other application componenents.
+ *
+ * \param [in] cfg  A pointer to the application configuration object
+ *
+ * \return 0 on success, or a negative vaue indicating error otheriwse
+ */
 ts_error_t
 ts_app_run(const struct ts_config *cfg);
 
+/*!
+ * \brief Free all resources held by the application
+ *
+ * This function prepares the application to be shutdown freeing all used
+ * resources by the application
+ */
 void
 ts_app_destroy(void);
 
--- src/app/include/app/callbacks.h
+++ src/app/include/app/callbacks.h
@@ -4,12 +4,37 @@
 #include "net/message.h"
 #include "net/tcp_client.h"
 
+/*!
+ * \brief A callback function to be called when a new client connects to the
+ *        the server
+ *
+ * This function is called whenever a new client connects to the server. This
+ * function is usually invoked by the application TCP listener
+ *
+ * \param [in] client  A pointer to the newly connected client
+ */
 void
 ts_on_client_connection(struct ts_tcp_client *client);
 
+/*!
+ * \brief A callback function to be called when a connected client disconnects
+ *        from the server
+ *
+ * This function is called whenever a connected client disconnects from  the
+ * server. This function is usually invoked by the application TCP listener
+ *
+ * \param [in] client  A pointer to the disconnected client
+ */
 void
 ts_on_client_disconnection(struct ts_tcp_client *client);
 
+/*!
+ * \brief A callback function to be called when a connected client sends
+ *        a request to the server
+ *
+ * \param [in] client  A pointer to the client which sent the request
+ * \param [in] req     A pointer to the sent request object
+ */
 void
 ts_on_client_request(struct ts_tcp_client *           client,
                      const struct ts_request_message *req);
