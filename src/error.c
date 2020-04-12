1f5e34275097faa23f2ab6e4b1326843f248b3e5
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-12 14:58:32 +0300

    Add basic error reporting functions

---
+++ src/error.c
@@ -0,0 +1,29 @@
+#include "error.h"
+
+#include <errno.h>
+#include <string.h>
+
+static const char *error_messages[] = {
+    "NULL arguments were passed to the function",
+    "An attempt to use uninitialized data",
+    "Could not allocate memory",
+    "No matching items were found",
+    "Index out of range",
+    "Not implemented",
+    "Could not open socket",
+    "Could not bind listener",
+    "Could not stop listener",
+    "Could not start event loop",
+    "The listener is already started",
+    "Could not get the address of the specified socket"
+};
+
+const char *
+ts_strerror(int err)
+{
+    if (err > TS_ERR_FIRST_MARKER && err < TS_ERR_LAST_MARKER) {
+        return error_messages[err + -TS_ERR_FIRST_MARKER - 1];
+    }
+
+    return strerror(err);
+}
