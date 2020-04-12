e07074f74cc2f24f9270e69215675e63a2a8fa72
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-12 17:55:10 +0300

    Implement a finite state machine for messages reading

--- src/net_memory.c
+++ src/net_memory.c
@@ -1,13 +1,39 @@
 #include "net/memory.h"
 #include "net/message.h"
+#include "net/read_context.h"
+#include "net/tcp_client.h"
 
 #include <uv.h>
 
 #include <stdlib.h>
 
 void
-ts_header_buffer_alloc_cb(uv_handle_t *handle, size_t s_size, uv_buf_t *buf)
+ts_read_buffer_alloc_cb(uv_handle_t *handle, size_t s_size, uv_buf_t *buf)
 {
-    buf->len  = TS_MESSAGE_REQUEST_HEADER_SIZE;
-    buf->base = malloc(TS_MESSAGE_REQUEST_HEADER_SIZE);
+    struct ts_tcp_client *client = (struct ts_tcp_client *)handle->data;
+
+    if (client->read_ctx.state == TS_READ_STATE_HEADER) {
+        if (!client->read_ctx.buf) {
+            client->read_ctx.buf =
+                (char *)malloc(TS_MESSAGE_REQUEST_HEADER_SIZE);
+            client->read_ctx.nread = 0;
+            buf->base              = (char *)client->read_ctx.buf;
+            buf->len               = TS_MESSAGE_REQUEST_HEADER_SIZE;
+        } else {
+            buf->base = client->read_ctx.buf + client->read_ctx.nread;
+            buf->len  = TS_MESSAGE_REQUEST_HEADER_SIZE - client->read_ctx.nread;
+        }
+    } else {
+        if (!client->read_ctx.buf) {
+            client->read_ctx.buf =
+                (char *)malloc(client->read_ctx.header.body_length);
+            client->read_ctx.nread = 0;
+            buf->base              = (char *)client->read_ctx.buf;
+            buf->len               = client->read_ctx.header.body_length;
+        } else {
+            buf->base = client->read_ctx.buf + client->read_ctx.nread;
+            buf->len =
+                client->read_ctx.header.body_length - client->read_ctx.nread;
+        }
+    }
 }
