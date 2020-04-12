3aaf9c250cbd109437f389f05bd1efb5b2f00553
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-12 15:00:10 +0300

    Add a function to get the representation form of an IP address

---
+++ src/net_address.c
@@ -0,0 +1,40 @@
+#include "net/address.h"
+
+#include "error.h"
+#include "log.h"
+
+#include <uv.h>
+
+#include <netinet/in.h>
+
+#include <errno.h>
+#include <string.h>
+
+ts_error_t
+ts_addr_to_string(uv_tcp_t *sock, char *buf, size_t len)
+{
+    int                     rc, addrlen;
+    struct sockaddr_storage addr;
+    const void *            addrptr;
+
+    addrlen = sizeof(addr);
+
+    if ((rc = uv_tcp_getsockname(sock, (struct sockaddr *)&addr, &addrlen)) <
+        0) {
+        log_debug("uv_tcp_getsockname: %s", uv_strerror(rc));
+        return TS_ERR_LOAD_ADDRESS_FAILED;
+    }
+
+    if (addr.ss_family == AF_INET) {
+        addrptr = (void *)&((struct sockaddr_in *)&addr)->sin_addr;
+    } else {
+        addrptr = (void *)&((struct sockaddr_in6 *)&addr)->sin6_addr;
+    }
+
+    if ((rc = uv_inet_ntop(addr.ss_family, addrptr, buf, len)) < 0) {
+        log_debug("inet_ntop: %s", uv_strerror(rc));
+        return TS_ERR_LOAD_ADDRESS_FAILED;
+    }
+
+    return TS_ERR_SUCCESS;
+}
