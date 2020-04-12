a080d934155de2090eea558c8a4e1d408a12aeff
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-12 22:18:37 +0300

    Improve buffer allocating callback function

--- src/net_memory.c
+++ src/net_memory.c
@@ -1,6 +1,6 @@
 #include "net/memory.h"
 #include "net/message.h"
-#include "net/read_context.h"
+#include "net/read_state_machine.h"
 #include "net/tcp_client.h"
 
 #include <uv.h>
@@ -12,28 +12,19 @@ ts_read_buffer_alloc_cb(uv_handle_t *handle, size_t s_size, uv_buf_t *buf)
 {
     struct ts_tcp_client *client = (struct ts_tcp_client *)handle->data;
 
-    if (client->read_ctx.state == TS_READ_STATE_HEADER) {
-        if (!client->read_ctx.buf) {
-            client->read_ctx.buf =
-                (char *)malloc(TS_MESSAGE_REQUEST_HEADER_SIZE);
-            client->read_ctx.nread = 0;
-            buf->base              = (char *)client->read_ctx.buf;
-            buf->len               = TS_MESSAGE_REQUEST_HEADER_SIZE;
-        } else {
-            buf->base = client->read_ctx.buf + client->read_ctx.nread;
-            buf->len  = TS_MESSAGE_REQUEST_HEADER_SIZE - client->read_ctx.nread;
-        }
+    size_t to_read;
+
+    if (client->read_sm.state == &ts_read_header_state) {
+        to_read = TS_MESSAGE_REQUEST_HEADER_SIZE;
     } else {
-        if (!client->read_ctx.buf) {
-            client->read_ctx.buf =
-                (char *)malloc(client->read_ctx.header.body_length);
-            client->read_ctx.nread = 0;
-            buf->base              = (char *)client->read_ctx.buf;
-            buf->len               = client->read_ctx.header.body_length;
-        } else {
-            buf->base = client->read_ctx.buf + client->read_ctx.nread;
-            buf->len =
-                client->read_ctx.header.body_length - client->read_ctx.nread;
-        }
+        to_read = client->read_sm.header.body_length;
     }
+
+    if (!client->read_sm.buf) {
+        client->read_sm.buf = malloc(to_read);
+        client->read_sm.nread = 0;
+    }
+
+    buf->base = client->read_sm.buf + client->read_sm.nread;
+    buf->len = to_read - client->read_sm.nread;
 }
