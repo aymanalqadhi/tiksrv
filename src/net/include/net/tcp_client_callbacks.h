5d72594510ac9b44bffdb7f3a1d442cb48d4ee5e
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-15 02:05:45 +0300

    Add callbacks for client sending and closing events

--- src/net/include/net/tcp_client_callbacks.h
+++ src/net/include/net/tcp_client_callbacks.h
@@ -5,7 +5,7 @@
 #include <uv.h>
 
 /*!
- * \brief A callback function to be called when the client has data ready
+ * \brief A callback function to be called when a client has data ready
  *
  * \param [in] socket  A pointer to the client socket
  * \param [in] nread   The read bytes count
@@ -13,5 +13,23 @@
  */
 void
 ts_tcp_client_read_cb(uv_stream_t *socket, ssize_t nread, const uv_buf_t *buf);
+
+/*!
+ * \brief A callback function to be called when a client has sent data
+ *
+ * \param [in] req     A pointer to the send request
+ * \param [in] status  The result of the send process
+ */
+void
+ts_tcp_client_write_cb(uv_write_t *req, int status);
+
+/*!
+ * \brief A callback function to be called when a client has closed its
+ *        connection
+ *
+ * \param [in] socket  A pointer to the client socket
+ */
+void
+ts_tcp_client_close_cb(uv_handle_t *socket);
 
 #endif
