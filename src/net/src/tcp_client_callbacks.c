777473dc3a218e57607d645a24a722a7e7a5d1ef
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-15 02:08:19 +0300

    Implement the sending and closing callback functions

--- src/net/src/tcp_client_callbacks.c
+++ src/net/src/tcp_client_callbacks.c
@@ -1,10 +1,10 @@
+#include "net/address.h"
 #include "net/memory.h"
 #include "net/message.h"
 #include "net/read_state_machine.h"
 #include "net/tcp_client.h"
 #include "net/tcp_client_callbacks.h"
 #include "net/tcp_listener.h"
-#include "net/address.h"
 
 #include "log/logger.h"
 
@@ -21,7 +21,32 @@ ts_tcp_client_read_cb(uv_stream_t *stream, ssize_t nread, const uv_buf_t *buf)
     if (nread < 0 || !buf->base) {
         ts_tcp_client_close(client);
         return;
-    } else {
-        (*client->read_sm.state)(client, nread);
     }
+
+    (*client->read_sm.state)(client, nread);
+}
+
+void
+ts_tcp_client_write_cb(uv_write_t *req, int status)
+{
+    int                   i;
+    struct ts_tcp_client *client;
+
+    client = (struct ts_tcp_client *)req->data;
+
+    ts_response_context_free(&client->response_ctx);
+    free(req);
+
+    if (status < 0) {
+        log_error("uv_write: %s", uv_strerror(status));
+        ts_tcp_client_close(client);
+        return;
+    }
+}
+
+void
+ts_tcp_client_close_cb(uv_handle_t *socket)
+{
+    struct ts_tcp_client *client = (struct ts_tcp_client *)socket->data;
+    (*client->listener->client_disconnect_cb)((uv_stream_t *)&client->socket);
 }
