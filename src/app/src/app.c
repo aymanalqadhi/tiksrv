4a199d86a5a78e560c62ffd45c9437fcf84f872f
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-15 01:59:38 +0300

    Add basic commands initialzation in the app startup function

--- src/app/src/app.c
+++ src/app/src/app.c
@@ -1,17 +1,33 @@
 #include "app/app.h"
+#include "app/shared.h"
 #include "app/callbacks.h"
 
+#include "commands/command.h"
 #include "config/config.h"
 #include "log/error.h"
 #include "log/logger.h"
-
 #include "net/address.h"
 #include "net/tcp_client.h"
 #include "net/tcp_listener.h"
+#include "util/memory.h"
 
 #include <stdlib.h>
 
 static struct ts_tcp_listener *listener;
+static struct ts_command *     commands;
+
+static void
+export_command(const struct ts_command *cmd)
+{
+    struct ts_command *tmp;
+
+    if (!(tmp = (struct ts_command *)ts_memdup(cmd, sizeof(*cmd)))) {
+        log_error("Could not add command with id #%d", cmd->id);
+        return;
+    }
+
+    HASH_ADD_INT(commands, id, tmp);
+}
 
 static ts_error_t
 app_init(const struct ts_config *cfg)
@@ -24,8 +40,13 @@ app_init(const struct ts_config *cfg)
         .on_request_cb       = &ts_on_client_request
     };
 
-    log_info("Creating a TCP listener on port %u...", cfg->listen_port);
+    log_info("Creating a TCP listener on port %u", cfg->listen_port);
     if ((rc = ts_tcp_listener_create(&listener, &app_cbs, cfg)) != 0) {
+        return rc;
+    }
+
+    log_info("Exporting system commands");
+    if ((rc = ts_commands_export(&export_command)) != 0) {
         return rc;
     }
 
@@ -60,4 +81,13 @@ ts_app_destroy(void)
 {
     ts_tcp_listener_stop(listener);
     free(listener);
+}
+
+struct ts_command *
+ts_app_get_command(uint32_t id)
+{
+    struct ts_command *out;
+    HASH_FIND_INT(commands, &id, out);
+
+    return out;
 }
