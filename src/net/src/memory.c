9cbdaf07df71d63940c8dc3f748b27da66630f90
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-15 20:00:06 +0300

    Prevent large body requests

--- src/net/src/memory.c
+++ src/net/src/memory.c
@@ -1,3 +1,4 @@
+#include "log/logger.h"
 #include "net/memory.h"
 #include "net/message.h"
 #include "net/read_state_machine.h"
@@ -17,14 +18,24 @@ ts_read_buffer_alloc_cb(uv_handle_t *handle, size_t s_size, uv_buf_t *buf)
     if (client->read_sm.state == &ts_read_header_state) {
         to_read = TS_MESSAGE_REQUEST_HEADER_SIZE;
     } else {
+        if (client->read_sm.header.body_length > TS_MAX_BODY_BUFFER_SIZE) {
+            log_warn(
+                "Client #%u has requested a large buffer (%u bytes, max: %u)",
+                client->id,
+                client->read_sm.header.body_length,
+                TS_MAX_BODY_BUFFER_SIZE);
+            buf->base = NULL;
+            buf->len  = 0;
+            return;
+        }
         to_read = client->read_sm.header.body_length;
     }
 
     if (!client->read_sm.buf) {
-        client->read_sm.buf = malloc(to_read);
+        client->read_sm.buf   = malloc(to_read);
         client->read_sm.nread = 0;
     }
 
     buf->base = client->read_sm.buf + client->read_sm.nread;
-    buf->len = to_read - client->read_sm.nread;
+    buf->len  = to_read - client->read_sm.nread;
 }
