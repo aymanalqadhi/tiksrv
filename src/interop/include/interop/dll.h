71ebad51cab2774f64b0348073efb315e2ce4f35
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-17 23:50:08 +0300

    Move dynamic-linked library handling to separate files

---
+++ src/interop/include/interop/dll.h
@@ -0,0 +1,10 @@
+#ifndef TIKSRV_INTRO_DYNAMIC_LINKING_H
+#define TIKSRV_INTRO_DYNAMIC_LINKING_H
+
+#include "interop/plugin.h"
+#include "log/error.h"
+
+ts_error_t
+ts_fill_plugins_symoles(struct ts_plugin *plug);
+
+#endif
