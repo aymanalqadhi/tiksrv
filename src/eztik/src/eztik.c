0780ec00740a4a0e49b8afeb2568c1a6eb4ab42b
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-21 00:10:06 +0300

    Rename eztik plugin main file from plugin.c to eztik.c

--- src/eztik/src/eztik.c
+++ src/eztik/src/eztik.c
@@ -1,24 +1,25 @@
 #include "commands/command.h"
+#include "config/config.h"
 #include "log/error.h"
 #include "log/logger.h"
 #include "services/container.h"
+
+#include <stdio.h>
+#include <stdlib.h>
 
 const char *ts_plugin_name    = "eztik";
 const char *ts_plugin_author  = "Ayman Al-Qadhi";
 const char *ts_plugin_version = "0.1 ALPHA";
 
 ts_error_t
-ts_plugin_init(struct ts_services_container *svcs)
+ts_plugin_init(const struct ts_config *cfg, struct ts_services_container *svcs)
 {
-    log_norm("Initializng eztik plugin");
-    printf("here am i\n");
     return TS_ERR_SUCCESS;
 }
 
 void
 ts_plugin_free(void)
 {
-    log_norm("Destroying eztik plugin");
 }
 
 ts_error_t
