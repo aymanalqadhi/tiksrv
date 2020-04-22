a22cbfb9727fd87afd8d4eb254028c1b740654ff
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-23 00:38:56 +0300

    Define 3 of the plugin commands

---
+++ src/eztik/include/eztik/commands.h
@@ -0,0 +1,17 @@
+#ifndef EZTIK_COMMANDS_H
+#define EZTIK_COMMANDS_H
+
+#include "commands/command.h"
+
+enum
+{
+    EZTIK_COMMAND_ROS_API_OPEN = 0x1000,
+    EZTIK_COMMAND_ROS_API_CLOSE,
+    EZTIK_COMMAND_ROS_VERSION
+};
+
+extern struct ts_command eztik_ros_api_open_command;
+extern struct ts_command eztik_ros_api_close_command;
+extern struct ts_command eztik_ros_version_command;
+
+#endif
