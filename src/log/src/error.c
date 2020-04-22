f41f59d12bb6b08b204da547492b22850f21e23e
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-22 23:42:24 +0300

    Add configuration and connection related errors

--- src/log/src/error.c
+++ src/log/src/error.c
@@ -9,21 +9,24 @@ static const char *error_messages[] = {
     "No matching items were found",
     "Index out of range",
     "Not implemented",
+    "Could not create socket",
     "Could not open socket",
     "Could not close socket",
     "Could not write to socket",
     "Could not bind listener",
     "Could not stop listener",
-    "Could not start event loop",
     "The listener is already started",
+    "Could not start event loop",
     "Could not get the address of the specified socket",
     "Could not encode response message header",
     "Could not export commands",
     "Could not load plugin",
+    "An attempt to load an invalid plugin",
     "Could not iterate directory entries",
     "Could not get current working directory",
     "Could not change directory",
-    "Could not parse configuration file"
+    "Could not parse configuration file",
+    "A required configuration value was not found in the configuration file"
 };
 
 const char *
