8199d4e4bb06bd2da3f4e0aa728140605d49e187
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-22 23:57:53 +0300

    Use the new command function signature

--- src/commands/src/echo_command.c
+++ src/commands/src/echo_command.c
@@ -9,32 +9,20 @@
 #include <stdint.h>
 
 static void
-echo_command_exec(struct ts_request *req, struct ts_services_container *svcs)
+echo_command_exec(const struct ts_request *req)
 {
     int                 rc;
     struct ts_response *resp;
 
-    if ((rc = ts_respone_create(&resp)) != 0) {
-        /* TODO:
-         * Terminate the connection
-         */
-        log_error("ts_respone_create: %s", ts_strerror(rc));
-        return;
-    }
+    resp = ts_respone_new(req);
 
-    if ((rc = ts_response_write(resp,
-                                ts_request_get_buffer(req),
-                                ts_request_get_length(req))) != 0) {
-        log_error("ts_response_write: %s", ts_strerror(rc));
-        goto cleanup;
-    }
+    ts_response_write(
+        resp, ts_request_get_buffer(req), ts_request_get_length(req));
 
-    if ((rc = ts_response_commit(resp, req)) != 0) {
+    if ((rc = ts_response_commit(resp)) != 0) {
         log_error("ts_response_commit: %s", ts_strerror(rc));
-        goto cleanup;
     }
 
-cleanup:
     ts_response_free(resp);
 }
 
