8fec1de58370e913bf8adc831904209fb41b935f
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-23 18:04:13 +0300

    Add new & free functions for the services container structure

--- src/services/include/services/container.h
+++ src/services/include/services/container.h
@@ -1,7 +1,22 @@
 #ifndef TIKSRV_SERVICES_CONTAINER_H
 #define TIKSRV_SERVICES_CONTAINER_H
 
+#include "services/hooks_manager.h"
+
+#include "log/error.h"
+
 struct ts_services_container
-{};
+{
+    struct ts_hooks_manager *hooks_manager;
+};
+
+struct ts_services_container *
+ts_services_container_new(void);
+
+ts_error_t
+ts_services_container_init(struct ts_services_container *container);
+
+void
+ts_services_container_free(struct ts_services_container *container);
 
 #endif
