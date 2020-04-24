5eb243f4dfed9e7820b10d5b0b676e52ce23977f
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-25 00:37:30 +0300

    Implement session closing command

--- src/eztik/src/ros_api_close_command.c
+++ src/eztik/src/ros_api_close_command.c
@@ -3,22 +3,40 @@
 #include "eztik/response.h"
 #include "eztik/session.h"
 
+#include "eztik/routeros/api.h"
+
 #include "commands/command.h"
 #include "commands/request.h"
 #include "commands/response.h"
 #include "log/error.h"
 #include "log/logger.h"
 
+#include <stdbool.h>
+
 static void
 ros_api_close_command_exec(const struct ts_request *req)
 {
+    int                 rc;
+    uint32_t            client_id;
     struct ts_response *resp;
 
-    resp = ts_respone_new(req);
+    client_id = ts_request_get_client_id(req);
+    resp      = ts_respone_new(req);
 
-    ts_response_write_string(resp, "Dummy Close");
-    ts_response_commit(resp);
+    if (eztik_has_session(client_id)) {
+        log_info("Closing session for client #%u", client_id);
+        eztik_remove_session(client_id);
+        ts_response_set_code(resp, TS_RESPONSE_CODE_SUCCESS);
+    } else {
+        log_warn("Client #%u with no open session sent a session close command",
+                 client_id);
+        ts_response_set_type(resp, EZTIK_COMMANDS_TYPE);
+        ts_response_set_code(resp, EZTIK_RESPONSE_SESSION_NOT_OPEN);
+    }
 
+    if ((rc = ts_response_commit(resp)) != 0) {
+        log_error("ts_response_commit: %s", ts_strerror(rc));
+    }
     ts_response_free(resp);
 }
 
