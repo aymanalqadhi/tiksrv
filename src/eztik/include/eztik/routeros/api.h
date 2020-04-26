2c1e9a7ef3774b472c60cc51a922c4cc2e61da52
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-27 00:03:46 +0300

    Remove unnecessary closing function, and change the name of requests sending function from ros_api_write to ros_api_send

--- src/eztik/include/eztik/routeros/api.h
+++ src/eztik/include/eztik/routeros/api.h
@@ -1,6 +1,8 @@
 #ifndef EZTIK_ROUTEROS_API_H
 #define EZTIK_ROUTEROS_API_H
 
+#include "eztik/routeros/api_sentence.h"
+
 #include "log/error.h"
 
 #include <glib.h>
@@ -11,7 +13,7 @@
 
 typedef void (*ros_api_open_cb)(bool, void *);
 typedef void (*ros_api_close_cb)(bool, void *);
-typedef void (*ros_api_write_cb)(bool, void *);
+typedef void (*ros_api_send_cb)(bool, struct ros_api_sentence *, void *);
 typedef void (*ros_api_free_cb)(void *);
 
 typedef enum
@@ -49,37 +51,33 @@ ts_error_t
 ros_api_open(struct ros_api *api, ros_api_open_cb cb, void *data);
 
 /*!
- * \brief Closes an already opened RouterOS API connection
+ * \brief Sends an API sentence to an opened RouterOS API connection
  *
- * \param [in, out] api   A pointer to the API connection to be closed
- * \param [in]      cb    A callback function pointer to be invoked when the
- *                        disconnection process is done
- * \param [in]      data  A pointer to be passed to the callback function
- *                        pointer when it gets invoked
- *
- * \return 0 on success, or a negative value indicating error otherwise
- */
-ts_error_t
-ros_api_close(struct ros_api *api, ros_api_close_cb, void *data);
-
-/*!
- * \brief Writes an API sentence to an opened RouterOS API connection
- *
- * \param [in] api        A pointer to the API connection into which to write
- * \param [in] sentence   A pointer to the API sentence which to be written
+ * \param [in] api        A pointer to the API connection which to send to
+ * \param [in] sentence   A pointer to the API sentence which to be sent
  * \param [in] cb         A callback function pointer to be invoked when the
- *                        write process is done
+ *                        send process is done
  * \param [in] data       A pointer to be passed to the callback function
  *                        pointer when it gets invoked
  *
  * \return 0 on success, or a negative value indicating error otherwise
  */
 ts_error_t
-ros_api_write(struct ros_api *           api,
-              const struct ros_sentence *sentence,
-              ros_api_write_cb           cb,
-              void *                     data);
+ros_api_send(struct ros_api *               api,
+             struct ros_api_sentence *sentence,
+             ros_api_send_cb               cb,
+             void *                         data);
 
+/*!
+ * \brief Frees the used resources used by a RouterOS API connection pointed to
+ *        by \see api closing the connection in the process
+ *
+ * \param [in, out] api   A pointer to the RouterOS API object to be freed
+ * \param [in]      cb    A callback function poiner to be called when the API
+ *                        connection is freed
+ * \param [in]      data  A pointer to a context data to be passed to the
+ *                        supplied callback function
+ */
 void
 ros_api_free(struct ros_api *api, ros_api_free_cb cb, void *data);
 
