ad292e3e12a5120f63720f4ca5f374673a9112c2
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-04-29 15:15:10 +0300

    Reimplement app configuration logic in C++

---
+++ src/app/include/app/config.hpp
@@ -0,0 +1,73 @@
+#ifndef TIKSRV_APP_CONFIG_HPP
+#define TIKSRV_APP_CONFIG_HPP
+
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/variables_map.hpp>
+
+#include <cstdint>
+#include <iostream>
+#include <string>
+#include <string_view>
+
+namespace ts::app {
+
+enum class config_key : std::uint32_t {
+    listen_port,
+    liten_backlog,
+    config_file,
+    help,
+    version
+};
+
+class config final {
+  public:
+    config();
+
+    inline bool has(const std::string &key) const {
+        return values_.count(key) > 0;
+    }
+
+    inline const boost::program_options::variable_value &
+    operator[](const std::string &key) const {
+        return values_[key];
+    }
+
+    inline void operator>>(std::ostream &os) {
+        os << desc_ << std::endl;
+    }
+
+    inline auto begin() const noexcept {
+        return values_.begin();
+    }
+
+    inline auto end() const noexcept {
+        return values_.end();
+    }
+
+    bool has(config_key key);
+    const boost::program_options::variable_value &
+         operator[](config_key key) const;
+    void parse_argv(int argc, const char *const argv[]);
+    void parse_config_file(std::string_view path);
+
+    static config from_argv(int argc, const char *const argv[]);
+
+    struct defaults {
+        static constexpr auto listen_port   = 3434u;
+        static constexpr auto liten_backlog = 100u;
+        static constexpr auto config_file   = "tiksrv.cfg";
+    };
+
+    struct constants {
+        static constexpr auto app_name    = "tiksrv";
+        static constexpr auto app_version = "0.1";
+    };
+
+  private:
+    boost::program_options::variables_map       values_;
+    boost::program_options::options_description desc_;
+};
+
+} // namespace ts::app
+
+#endif
---
+++ src/app/src/config.cpp
@@ -0,0 +1,60 @@
+#include "app/config.hpp"
+
+#include <boost/program_options.hpp>
+
+#include <array>
+#include <stdexcept>
+#include <string_view>
+
+namespace po = boost::program_options;
+
+namespace {
+
+constexpr std::array config_string_keys {"port", "backlog", "config", "help",
+                                         "version"};
+
+} // namespace
+
+namespace ts::app {
+
+config::config() : desc_ {"Allowed Options"} {
+    desc_.add_options()(
+        "port,p",
+        po::value<std::uint16_t>()->default_value(defaults::listen_port),
+        "The server listening port");
+    desc_.add_options()(
+        "backlog,b",
+        po::value<std::uint32_t>()->default_value(defaults::liten_backlog),
+        "The maximum number in the listening queue");
+    desc_.add_options()(
+        "config,c",
+        po::value<std::string>()->default_value(defaults::config_file),
+        "The configuration file location");
+    desc_.add_options()("help,h", "Show help message");
+    desc_.add_options()("version,V", "Show app version");
+}
+
+void config::parse_argv(int argc, const char *const argv[]) {
+    po::store(po::parse_command_line(argc, argv, desc_), values_);
+    po::notify(values_);
+}
+
+void config::parse_config_file(std::string_view path) {
+    po::store(po::parse_config_file(path.data(), desc_), values_);
+}
+
+bool config::has(config_key key) {
+    return has(::config_string_keys[static_cast<std::uint32_t>(key)]);
+}
+
+const po::variable_value &config::operator[](config_key key) const {
+    return operator[](::config_string_keys[static_cast<std::uint32_t>(key)]);
+}
+
+config config::from_argv(int argc, const char *const argv[]) {
+    config ret {};
+    ret.parse_argv(argc, argv);
+    return ret;
+}
+
+} // namespace ts::app
