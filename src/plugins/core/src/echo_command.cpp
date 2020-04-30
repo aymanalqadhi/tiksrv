bc2366a51d65f0b4b9ea2efe85bdcdd81ba9bffd
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-01 02:47:28 +0300

    Use the new respond method overloads

--- src/plugins/core/src/echo_command.cpp
+++ src/plugins/core/src/echo_command.cpp
@@ -4,17 +4,10 @@
 
 #include <memory>
 
-namespace ts::plugins::core::commands
-
-{
+namespace ts::plugins::core::commands {
 
 void echo_command::operator()(client_ptr client, ts::net::request &&req) {
-    auto resp_ptr = std::make_shared<ts::net::response>();
-
-    resp_ptr->body(req.body());
-    resp_ptr->tag(req.header().tag);
-
-    client->respond(std::move(resp_ptr));
+    client->respond(req.body(), req.header().tag);
 }
 
 } // namespace ts::plugins::core::commands
