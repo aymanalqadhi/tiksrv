f6557b12e321eeea49bc5e749bb1807442bef799
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-01 23:31:10 +0300

    Implement the services manager service

---
+++ src/services/src/services_manager.cpp
@@ -0,0 +1,37 @@
+#include "services/services_manager.hpp"
+
+#include "fmt/format.h"
+
+#include <stdexcept>
+
+namespace ts::services {
+
+void services_manager::initialize() {
+    logger_.info("Initializing services");
+
+    if (state_.load() != services_manager_state::uninitialized) {
+        throw std::runtime_error {"Service have been already initialized"};
+    }
+
+    state_.store(services_manager_state::initialized);
+
+    for (const auto &svc : services_) {
+        try {
+            logger_.debug("Initializing service `{} ({})'",
+                          svc.second->display_name(), svc.first);
+        } catch (const std::exception &ex) {
+            if (svc.second->is_required()) {
+                throw std::runtime_error {
+                    fmt::format("Could not initialize required service `{}'",
+                                svc.second->display_name())};
+            }
+
+            logger_.warn("Could not initialize optioal service `{}', the "
+                         "service will not be available",
+                         svc.second->display_name());
+            services_.erase(svc.first);
+        }
+    }
+}
+
+} // namespace ts::services
