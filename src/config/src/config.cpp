c8a5f80d291c79eedb21bd5a8c620293182db67b
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-02 22:03:47 +0300

    Allow unregistered options when parsing configuration files

--- src/config/src/config.cpp
+++ src/config/src/config.cpp
@@ -45,7 +45,7 @@ void config::parse_argv(int argc, const char *const argv[]) {
 
 void config::parse_config_file(
     std::string_view path, boost::program_options::options_description &desc) {
-    po::store(po::parse_config_file(path.data(), desc), values_);
+    po::store(po::parse_config_file(path.data(), desc, true), values_);
 }
 
 auto config::has(config_key key) const -> bool {
