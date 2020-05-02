ef29b08bb997ed94a09c6b662cab7b5bbdb1a54e
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-02 17:20:34 +0300

    Add a file with plugin confiuration keys and defaults

---
+++ src/plugins/eztik/include/eztik/config.hpp
@@ -0,0 +1,20 @@
+#ifndef EZTIK_CONFIG_HPP
+#define EZTIK_CONFIG_HPP
+
+namespace eztik {
+
+struct config_keys {
+    static constexpr auto ros_ip       = "eztik.ros_ip";
+    static constexpr auto ros_api_port = "eztik.ros_api_port";
+};
+
+struct config_defaults {
+    static constexpr auto ros_ip           = "127.0.0.1";
+    static constexpr auto ros_api_port     = 8728U;
+    static constexpr auto ros_api_user     = "admin";
+    static constexpr auto ros_api_password = "";
+};
+
+} // namespace eztik
+
+#endif
