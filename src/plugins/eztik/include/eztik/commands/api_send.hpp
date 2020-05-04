8fbd3a00d0b0972f454f3d198910c3b9d543ee81
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-04 16:17:49 +0300

    Add a basic implementation for an API sentences sending command

---
+++ src/plugins/eztik/include/eztik/commands/api_send.hpp
@@ -0,0 +1,39 @@
+#ifndef EZTIK_COMMANDS_API_SEND_HPP
+#define EZTIK_COMMANDS_API_SEND_HPP
+
+#include "eztik/services/sessions.hpp"
+
+#include "interop/command.hpp"
+#include "log/logger.hpp"
+
+#include <string>
+
+namespace eztik::commands {
+
+class api_send_command final : public ts::interop::command {
+    using client_ptr = std::shared_ptr<ts::net::tcp_client>;
+
+  public:
+    api_send_command(ts::log::logger &                  logger,
+                     eztik::services::sessions_service &sessions_svc)
+        : name_ {"RouterOS API Send"},
+          logger_ {logger},
+          sessions_svc_ {sessions_svc} {
+    }
+
+    auto name() const noexcept -> const std::string & override {
+        return name_;
+    }
+
+    void operator()(client_ptr client, ts::net::request &&req) override;
+
+  private:
+    std::string name_;
+
+    ts::log::logger &                  logger_;
+    eztik::services::sessions_service &sessions_svc_;
+};
+
+} // namespace eztik::commands
+
+#endif
---
+++ src/plugins/eztik/src/api_send_command.cpp
@@ -0,0 +1,62 @@
+#include "eztik/commands/api_send.hpp"
+#include "eztik/response.hpp"
+#include "eztik/routeros/api.hpp"
+#include "eztik/routeros/sentence.hpp"
+
+#include "net/message.hpp"
+
+#include "spdlog/fmt/bin_to_hex.h"
+#include <boost/algorithm/string.hpp>
+
+#include <string>
+#include <vector>
+
+using boost::system::error_code;
+using eztik::session;
+using eztik::routeros::response_sentence;
+
+namespace eztik::commands {
+
+void api_send_command::operator()(client_ptr client, ts::net::request &&req) {
+    if (!sessions_svc_.has(client->id())) {
+        logger_.warn(
+            "Client #{} with no open sessions tried to send an API message",
+            client->id());
+        client->respond(eztik::make_response_code(
+                            eztik::response_code::ros_session_not_open),
+                        req.header().tag);
+        return;
+    }
+
+    ///
+    /// TODO:
+    /// COMPLETE IMPLEMENTATION
+    ///
+    auto session = sessions_svc_[client->id()];
+
+    std::vector<std::string> words {};
+
+    auto body = req.body();
+    body.erase(0, 4);
+
+    if (words.size() == 0) {
+        client->respond(ts::net::response_code::invalid_request,
+                        req.header().tag);
+        return;
+    }
+
+    eztik::routeros::request_sentence sent {body, req.header().tag};
+
+    session->send(
+        sent,
+        [this](const error_code &err, eztik::session &s,
+               response_sentence &&resp) {
+            logger_.fatal("{}", err.message());
+            for (const auto &p : resp) {
+                logger_.fatal("{} ===> {}", p.first, p.second);
+            }
+        },
+        true);
+}
+
+} // namespace eztik::commands
