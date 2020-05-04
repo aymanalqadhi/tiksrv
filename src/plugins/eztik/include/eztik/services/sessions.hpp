461e3fca8c583b91284adb8d85109a596dbd2136
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-05 00:36:33 +0300

    Attach a closing callback to session objects

--- src/plugins/eztik/include/eztik/services/sessions.hpp
+++ src/plugins/eztik/include/eztik/services/sessions.hpp
@@ -19,6 +19,9 @@ namespace eztik::services {
 class session_handler;
 
 class sessions_service final : public eztik::session_handler {
+    using session_open_cb  = std::function<void(std::shared_ptr<session>)>;
+    using session_close_cb = std::function<void(std::shared_ptr<session>)>;
+
   public:
     sessions_service(const ts::config::config &                   conf,
                      boost::asio::io_context &                    io,
@@ -31,26 +34,27 @@ class sessions_service final : public eztik::session_handler {
         setup_hooks();
     }
 
+    inline auto operator[](std::uint32_t id) noexcept
+        -> std::shared_ptr<session> {
+        return sessions_[id].first;
+    }
+
     inline auto has(std::uint32_t id) const noexcept -> bool {
         return sessions_.contains(id);
     }
 
     inline auto has_ready(std::uint32_t id) const noexcept -> bool {
-        return has(id) && sessions_.at(id)->is_ready();
+        return has(id) && sessions_.at(id).first->is_ready();
     }
 
-    inline auto operator[](std::uint32_t id) noexcept
-        -> std::shared_ptr<session> {
-        return sessions_[id];
-    }
-
-    void create(std::uint32_t                                 id,
-                std::function<void(std::shared_ptr<session>)> cb);
+    void create(std::uint32_t    id,
+                session_open_cb  open_cb,
+                session_close_cb close_cb);
 
     void close(std::uint32_t id);
 
   public:
-    void on_close(const eztik::session& s) override;
+    void on_close(const eztik::session &s) override;
 
   private:
     void setup_hooks();
@@ -61,7 +65,9 @@ class sessions_service final : public eztik::session_handler {
     ts::log::logger &                            logger_;
     std::shared_ptr<ts::services::hooks_manager> hooks_manager_;
 
-    std::map<std::uint32_t, std::shared_ptr<eztik::session>> sessions_;
+    std::map<std::uint32_t,
+             std::pair<std::shared_ptr<eztik::session>, session_close_cb>>
+        sessions_;
 };
 
 } // namespace eztik::services
