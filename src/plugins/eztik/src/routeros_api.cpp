96fd1f8bc06e056a7fc8e88d19ee169b20e8833a
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-04 16:14:09 +0300

    Implemen API responses reading logic

--- src/plugins/eztik/src/routeros_api.cpp
+++ src/plugins/eztik/src/routeros_api.cpp
@@ -2,8 +2,16 @@
 
 #include <boost/asio/error.hpp>
 #include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/placeholders.hpp>
+#include <boost/asio/read.hpp>
+#include <boost/system/error_code.hpp>
 
 #include <cassert>
+#include <iostream>
+
+using boost::asio::transfer_exactly;
+using boost::asio::placeholders::bytes_transferred;
+using boost::asio::placeholders::error;
 
 namespace ip = boost::asio::ip;
 
@@ -12,8 +20,6 @@ namespace eztik::routeros {
 void api::open(const std::string &    host,
                std::uint16_t          port,
                const connect_handler &cb) {
-    assert(!is_open());
-
     try {
         ip::tcp::endpoint ep {ip::make_address(host), port};
         sock_.async_connect(ep, cb);
@@ -22,7 +28,7 @@ void api::open(const std::string &    host,
     }
 }
 
-void api::send(const sentence &s, const send_handler &cb) {
+void api::send(const request_sentence &s, const send_handler &cb) {
     assert(is_open());
 
     buffer_.clear();
@@ -32,19 +38,110 @@ void api::send(const sentence &s, const send_handler &cb) {
 }
 
 void api::close() {
-    assert(is_open());
-    sock_.close();
+    if (sock_.is_open()) {
+        sock_.close();
+    }
+    state(read_state::closed);
 }
 
 void api::start() {
     assert(sock_.is_open());
-    assert(!running_.load());
+    assert(state() == read_state::idle);
 
-    handler_.on_response({""});
+    read_next_word();
 }
 
 void api::stop() {
-    assert(running_.load());
+    if (state() == read_state::closed) {
+        return;
+    }
+
+    close();
+}
+
+void api::read_next(std::size_t n) {
+    context().buffer().resize(n);
+
+    boost::asio::async_read(
+        sock_, boost::asio::buffer(context().buffer(), n), transfer_exactly(n),
+        [this](const auto &err, auto nread) {
+            handle_read(err, {context().buffer().c_str(), nread});
+        });
+}
+
+void api::read_next_word() {
+    if (state() == read_state::closed) {
+        return;
+    }
+
+    assert(state() == read_state::idle);
+
+    state(read_state::reading_length);
+    read_next(1);
+}
+
+void api::on_reading_length(std::string_view data) {
+    assert(state() == read_state::reading_length);
+    assert(data.size() == 1);
+
+    std::uint8_t fbyte {static_cast<std::uint8_t>(data[0])};
+    std::uint8_t mbytes {};
+
+    if (fbyte == 0) {
+        handler_.on_response(context().current_sentence());
+        context().current_sentence().clear();
+
+        state(read_state::idle);
+        read_next_word();
+    } else if ((fbyte & 0x80) != 0x00) {
+        if ((fbyte & 0xC0) == 0x80) {
+            fbyte &= ~0xC0;
+            mbytes = 1;
+        } else if ((fbyte & 0xE0) == 0xC0) {
+            fbyte &= ~0xE0;
+            mbytes = 2;
+        } else {
+            if ((fbyte & 0xF0) == 0xE0) {
+                fbyte &= ~0xF0;
+            }
+            mbytes = 3;
+        }
+
+        boost::asio::async_read(
+            sock_, boost::asio::buffer(context().buffer(), mbytes),
+            transfer_exactly(mbytes),
+            [this, fbyte, mbytes](const auto &err, auto nread) {
+                assert(nread == mbytes);
+                if (err) {
+                    on_error(err);
+                    return;
+                }
+
+                std::size_t word_len {fbyte};
+
+                for (std::size_t i {0}; i < mbytes; ++i) {
+                    word_len = (word_len << 8) | (context().buffer()[i] & 0xFF);
+                }
+
+                state(read_state::reading_word);
+                read_next(word_len);
+            });
+
+    } else {
+        state(read_state::reading_word);
+        read_next(fbyte);
+    }
+}
+void api::on_reading_word(std::string_view data) {
+    assert(state() == read_state::reading_word);
+
+    context().current_sentence().push(std::string {data});
+    state(read_state::idle);
+    read_next_word();
+}
+
+void api::on_error(const boost::system::error_code &err) {
+    handler_.on_error(err);
 }
 
 } // namespace eztik::routeros
