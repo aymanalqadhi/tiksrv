e65759a862d715aa4ffad9d024a1b2ae420accee
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-04 18:15:20 +0300

    Check for pending sessions when opening a new one

--- src/plugins/eztik/src/api_open_command.cpp
+++ src/plugins/eztik/src/api_open_command.cpp
@@ -1,36 +1,43 @@
 #include "eztik/commands/api_open.hpp"
 #include "eztik/response.hpp"
 #include "eztik/routeros/api.hpp"
+#include "net/message.hpp"
 
 #include "net/message.hpp"
 
 namespace eztik::commands {
 
 void api_open_command::execute(client_ptr client, ts::net::request &&req) {
-    if (sessions_svc_.has(client->id())) {
+    if (sessions_svc_.has_ready(client->id())) {
         logger_.warn(
             "Client #{} with an already open session has requested a new one",
             client->id());
         client->respond(eztik::make_response_code(
                             eztik::response_code::ros_session_already_open),
                         req.header().tag);
-        return;
+    } else if (sessions_svc_.has(client->id())) {
+        logger_.warn("Client #{} sent multiple session opening requests",
+                     client->id());
+        client->respond(ts::net::response_code::operation_pending,
+                        req.header().tag);
+    } else {
+        sessions_svc_.create(
+            client->id(), [this, client, tag = req.header().tag](auto session) {
+                if (session) {
+                    logger_.info(
+                        "A new session was opened successfully for client #{}",
+                        client->id());
+                    client->respond(ts::net::response_code::success, tag);
+                } else {
+                    logger_.error("Could not open session for client #{}",
+                                  client->id());
+                    client->respond(
+                        eztik::make_response_code(
+                            eztik::response_code::ros_session_open_failed),
+                        tag);
+                }
+            });
     }
-
-    sessions_svc_.create(client->id(), [this, client,
-                                        tag = req.header().tag](auto session) {
-        if (session) {
-            logger_.info("A new session was opened successfully for client #{}",
-                         client->id());
-            client->respond(ts::net::response_code::success, tag);
-        } else {
-            logger_.error("Could not open session for client #{}",
-                          client->id());
-            client->respond(eztik::make_response_code(
-                                eztik::response_code::ros_session_open_failed),
-                            tag);
-        }
-    });
 }
 
 } // namespace eztik::commands
