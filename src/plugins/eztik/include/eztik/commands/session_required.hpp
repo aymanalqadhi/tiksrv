6bc50fd0759e946ea404ed63b0bedf5b62fda4ae
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-04 18:12:56 +0300

    Add a field to store the current session

--- src/plugins/eztik/include/eztik/commands/session_required.hpp
+++ src/plugins/eztik/include/eztik/commands/session_required.hpp
@@ -7,6 +7,8 @@
 #include "interop/command.hpp"
 #include "log/logger.hpp"
 
+#include <memory>
+
 namespace eztik::commands {
 
 class session_required_command : public ts::interop::command {
@@ -14,8 +16,8 @@ class session_required_command : public ts::interop::command {
 
   public:
     session_required_command(ts::log::logger &                  logger,
-                    eztik::services::sessions_service &sessions_svc)
-        : logger_ {logger}, sessions_svc_ {sessions_svc} {
+                             eztik::services::sessions_service &sessions_svc)
+        : logger_ {logger}, sessions_svc_ {sessions_svc}, session_ {nullptr} {
     }
 
     void operator()(client_ptr client, ts::net::request &&req) override;
@@ -23,6 +25,7 @@ class session_required_command : public ts::interop::command {
   protected:
     ts::log::logger &                  logger_;
     eztik::services::sessions_service &sessions_svc_;
+    std::shared_ptr<eztik::session>    session_;
 };
 
 } // namespace eztik::commands
