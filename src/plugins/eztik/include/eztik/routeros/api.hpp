05a32d6e20d04f6cf8f451de27570b51bc9f804a
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-04 16:12:48 +0300

    Add an event handler for API events

--- src/plugins/eztik/include/eztik/routeros/api.hpp
+++ src/plugins/eztik/include/eztik/routeros/api.hpp
@@ -1,12 +1,12 @@
 #ifndef EZTIK_ROUTEROS_API_HPP
 #define EZTIK_ROUTEROS_API_HPP
 
+#include "eztik/routeros/api_read_state_machine.hpp"
 #include "eztik/routeros/sentence.hpp"
 
 #include <boost/asio/ip/tcp.hpp>
 #include <boost/system/error_code.hpp>
 
-#include <atomic>
 #include <string>
 #include <vector>
 
@@ -15,11 +15,11 @@ namespace eztik::routeros {
 class api_handler {
   public:
     virtual void on_error(const boost::system::error_code &err) = 0;
-    virtual void on_close();
-    virtual void on_response(const sentence &resp);
+    virtual void on_close()                                     = 0;
+    virtual void on_response(const sentence &resp)              = 0;
 };
 
-class api final {
+class api final : public api_read_state_machine {
     using connect_handler =
         std::function<void(const boost::system::error_code &)>;
     using send_handler = std::function<void(const boost::system::error_code &,
@@ -27,20 +27,15 @@ class api final {
 
   public:
     api(boost::asio::io_context &io, api_handler &handler)
-        : sock_ {io}, handler_ {handler}, running_ {false} {
+        : sock_ {io}, handler_ {handler} {
     }
 
     api(api &&rh) : sock_ {std::move(rh.sock_)}, handler_ {rh.handler_} {
-        running_.store(rh.running_);
     }
 
     api(const api &rh) = delete;
 
     ~api() {
-        if (is_running()) {
-            stop();
-        }
-
         if (is_open()) {
             close();
         }
@@ -52,24 +47,28 @@ class api final {
 
     void close();
 
-    void send(const sentence &s, const send_handler &cb);
+    void send(const request_sentence &s, const send_handler &cb);
 
     void start();
 
     void stop();
 
-    inline auto is_running() const noexcept -> bool {
-        return running_.load();
-    }
-
     inline auto is_open() const noexcept -> bool {
         return sock_.is_open();
     }
 
+  public:
+    void on_reading_length(std::string_view data) override;
+    void on_reading_word(std::string_view data) override;
+    void on_error(const boost::system::error_code &err) override;
+
+  private:
+    void read_next(std::size_t n);
+    void read_next_word();
+
   private:
     std::vector<std::uint8_t>    buffer_;
     boost::asio::ip::tcp::socket sock_;
-    std::atomic_bool             running_;
     api_handler &                handler_;
 };
 
