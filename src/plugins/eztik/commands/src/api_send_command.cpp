d0810d8bb92ef5b17799482012f10d0a174a8a1d
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-15 06:17:42 +0300

    Reimplement the raw API message sending command

--- src/plugins/eztik/commands/src/api_send_command.cpp
+++ src/plugins/eztik/commands/src/api_send_command.cpp
@@ -4,40 +4,61 @@
 #include "eztik/routeros/api.hpp"
 #include "eztik/routeros/sentence.hpp"
 
+#include "impl/proto/api_send.pb.h"
+
 #include "net/message.hpp"
 
-#include "spdlog/fmt/bin_to_hex.h"
 #include <boost/algorithm/string.hpp>
 
 #include <string>
 #include <vector>
 
 using boost::system::error_code;
-using eztik::services::session;
+
 using eztik::routeros::response_sentence;
+using eztik::services::session;
 
 namespace eztik::commands {
 
 void api_send_command::execute(client_ptr client, ts::net::request &&req) {
-    ///
-    /// TODO:
-    /// COMPLETE IMPLEMENTATION
-    ///
-    std::vector<std::string> words {};
+    eztik::commands::messages::ApiRequest msg {};
 
-    auto body = req.body();
-    body.erase(0, 4);
+    if (!msg.ParseFromString(req.body())) {
+        logger_.error("Client #{} sent an invalid API request", client->id());
+        client->respond(ts::net::response_code::invalid_request,
+                        req.header().tag);
+        return;
+    }
 
-    auto sent = session_->api()->make_request(std::move(body));
+    auto sentence = session_->api()->make_request(msg.command());
+
+    for (const auto &word : msg.params()) {
+        sentence->push(word);
+    }
 
     session_->api()->send(
-        sent,
-        [this](const error_code &err, eztik::routeros::api &api,
-               response_sentence &&resp) {
-            logger_.fatal("{}", err.message());
-            for (const auto &p : resp) {
-                logger_.fatal("{} ===> {}", p.first, p.second);
+        sentence,
+        [this, client {std::move(client)}, tag = req.header().tag](
+            const error_code &err, eztik::routeros::api &api,
+            response_sentence &&resp) {
+            if (err) {
+                logger_.error("Could not send API message for client #{}: {}",
+                              session_->id(), err.message());
+                client->respond(ts::net::response_code::unknown_error, tag);
+                return;
             }
+
+            eztik::commands::messages::ApiResponse msg {};
+            msg.set_type(static_cast<
+                         eztik::commands::messages::ApiResponse_ResponseType>(
+                resp.type()));
+
+            for (auto &[key, value] : resp) {
+                msg.mutable_params()->operator[](std::move(key)) =
+                    std::move(value);
+            }
+
+            client->respond(msg.SerializeAsString(), tag);
         },
         true);
 }
