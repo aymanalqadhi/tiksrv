99d7ce32f0373e950bd5a6cf88e82ee7cc7e9c07
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-17 07:17:25 +0300

    Add a command to load a specific RouterOS table items

--- src/plugins/eztik/routeros/include/eztik/routeros/commands.hpp
+++ src/plugins/eztik/routeros/include/eztik/routeros/commands.hpp
@@ -15,13 +15,13 @@ struct login1 : request_sentence {
     static constexpr auto command = "/login";
 };
 
-struct login2 : login1 {
+struct login2 final : login1 {
     login2(std::uint32_t      tag,
-           std::string        name,
+           const std::string &name,
            const std::string &password,
            const std::string &cha)
         : login1 {tag} {
-        push_param(name_param, std::move(name));
+        push_param(name_param, name);
         push_param(password_param, "00{}", hash_password(password, cha));
     }
 
@@ -29,6 +29,14 @@ struct login2 : login1 {
     static constexpr auto password_param  = "response";
     static constexpr auto challenge_param = "ret";
     static constexpr auto challenge_size  = 32UL;
+};
+
+struct getall : request_sentence {
+    getall(std::uint32_t tag, const std::string &path)
+        : request_sentence {path + command_suffix, tag} {
+    }
+
+    static constexpr auto command_suffix = "/getall";
 };
 
 } // namespace eztik::routeros::commands
