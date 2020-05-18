af5dcd650be8b34edb371b14c8d310d6bfec202a
Ayman Al-Qadhi <aymanqadi2012@gmail.com>
2020-05-18 12:35:35 +0300

    Use more modern type traits instead of classic SFINAE

--- src/services/include/services/service.hpp
+++ src/services/include/services/service.hpp
@@ -11,28 +11,23 @@ namespace ts::services {
 
 class service {
   public:
-    virtual auto display_name() const noexcept -> const std::string & = 0;
-    virtual auto is_required() const noexcept -> bool                 = 0;
+    [[nodiscard]] virtual auto display_name() const noexcept
+        -> const std::string &                                      = 0;
+    [[nodiscard]] virtual auto is_required() const noexcept -> bool = 0;
 
     virtual void initialize() = 0;
 };
 
+template <typename, typename = std::void_t<>>
+struct has_name_member : std::false_type {};
+
 template <typename T>
-class is_service {
-    using one = char;
+struct has_name_member<T, std::void_t<typename T::name>> : std::true_type {};
 
-    struct two {
-        char x[2];
-    };
-
-    template <typename C>
-    static constexpr one test(typeof(&C::name));
-    template <typename C>
-    static constexpr two test(...);
-
-  public:
-    static constexpr auto value = std::is_base_of<service, T>::value &&
-                                  sizeof(test<T>(0)) == sizeof(char);
+template <typename T>
+struct is_service {
+    static constexpr auto value =
+        std::is_base_of<service, T>::value && has_name_member<T>::value;
 };
 
 } // namespace ts::services
